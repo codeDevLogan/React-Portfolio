f720ff84686b97239c4def8009ecb6fde11da172
codeDevLogan <logandevscode@gmail.com>
2023-03-09 20:50:50 -0700

    Saving work. I now have a github link for the seperate projects linked on the projects.

--- src/components/Biopage/index.js
+++ src/components/Biopage/index.js
@@ -1,4 +1,14 @@
 import '../../styles/biopage.css';
+const RESUME_PDF_URL = '/Logan_Peterson_Resume.pdf'
+const downloadFileAtURL = (url) => {
+    const aTag = document.createElement('a');
+    const fileName = url.split('/').pop();
+    aTag.href = url;
+    aTag.setAttribute('download',fileName);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+}
 
 export default function Biopage() {
     return (
@@ -13,7 +23,7 @@ export default function Biopage() {
                     </div>
                     <div className='back'>
                         <h1>Logan Peterson</h1>
-                        <p>Welcome to my Portfolio site. This site was built with React and Bootstrap 5, and is designed to showcase my work and my abilities as a Web Development Professional. Hover over the other card to see my skills.</p>
+                        <p>Welcome to my Portfolio site. This site was built with React and Bootstrap 5, and is designed to showcase my work and my abilities as a Web Development Professional. Hover over the other cards to see more.</p>
                     </div>
                 </div>
             </div>
@@ -27,7 +37,22 @@ export default function Biopage() {
                     </div>
                     <div className='back'>
                         <h1>Skills</h1>
-                        <p>Logan Peterson is based in Tucson, Arizona. He is a Full Stack Web Developer, well versed in the following skills, HTML5, CSS, JS, the MERN stack, React.JS, NodeJS, Handlebars.js, Agile Development Principles, Git, GitHub, Restful APIs, Java, Heroku Deployment. Hover over the other card to see more.</p>
+                        <p>Logan Peterson is based in Tucson, Arizona. He is a Full Stack Web Developer, well versed in the following skills, HTML5, CSS, JS, the MERN stack, React.JS, NodeJS, Handlebars.js, Agile Development Principles, Git, GitHub, Restful APIs, Java, Heroku Deployment. Hover over the other cards to see more.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='container2'>
+                <div className='card'>
+                    <div className='front'>
+                        <img id="Biopic3" src="/images/Handshake.jpg" alt="Two Business Men Shaking Hands."/>
+                        <h1>
+                            Resume
+                        </h1>
+                    </div>
+                    <div className='back'>
+                        <h1>Resume</h1>
+                        <p>Would you like to review my professional Web-Development Resume? Click below to Download Now!</p>
+                        <button onClick={() => {downloadFileAtURL(RESUME_PDF_URL)}}>Download Resume</button>
                     </div>
                 </div>
             </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,13 +1,18 @@
 import '../../styles/projects.css'
 import IndividualProject from './individualProject'
+import GitHubAnchor from './gitHubAnchors'
 
 export default function Projects() {
     return (
         <>
             <section id="projects" className="projectContainer">
-                <IndividualProject position={1} projectName={"Note Taker App"} projectLink="https://ancient-stream-13435.herokuapp.com" imgSrc={"/images/NoteTaker.png"}/>
-                <IndividualProject position={2} projectName={"New Day, New City"} projectLink="https://anthonyfrederick7.github.io/New-Day-New-City/"/>
-                <IndividualProject position={3} projectName={"Random Password Generator"} projectLink="https://codedevlogan.github.io/Random-Pass-Gen/"/>
+                <p className='text-center'><h1>Projects</h1></p>
+                <IndividualProject position={1} projectName={"Note Taker App"} projectLink="https://ancient-stream-13435.herokuapp.com" imgSrc={"/images/NoteTaker.png"} />
+                <GitHubAnchor gitLink={"https://github.com/codeDevLogan/Note-Taker-App"} />
+                <IndividualProject position={2} projectName={"New Day, New City"} projectLink="https://anthonyfrederick7.github.io/New-Day-New-City/" imgSrc={"/images/New-Day-New-City_.png"} gitLink={"https://github.com/AnthonyFrederick7/New-Day-New-City"}/>
+                <GitHubAnchor gitLink={"https://github.com/AnthonyFrederick7/New-Day-New-City"} />
+                <IndividualProject position={3} projectName={"Random Password Generator"} projectLink="https://codedevlogan.github.io/Random-Pass-Gen/" imgSrc={"/images/Random-Pass-Gen.png"} gitLink={"https://github.com/codeDevLogan/Random-Pass-Gen"}/>
+                <GitHubAnchor gitLink={"https://github.com/codeDevLogan/Random-Pass-Gen"} />
             </section>
         </>
     )
